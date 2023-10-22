4a105c9d16d905c210ec5e67e138368377fe0061
fareed02 <fareedevs@gmail.com>
2023-10-22 22:45:29 +0500

    my mobile app

---
+++ app.js
@@ -0,0 +1,274 @@
+var mobileCompanies = {
+    iphone: {
+        iphone15: {
+            image: "images/iphone-15.jpeg",
+            name: "iphone 15",
+            about: "iPhone 15 Pro is the first iPhone to support USB 3, 6 for a huge leap in data transfer speeds and faster pro workflows than ever before.",
+            manufacter: "Manufactured by iphone",
+            battery: "Battery Timing Approx 2 days",
+            price: "12 Lakh in PKR",
+        },
+        iphone14: {
+            image: "images/iphone-14.jpg",
+            name: "iphone 14",
+            about: "A new, larger 6.7-inch size joins the popular 6.1-inch design, featuring a new dual-camera system, Crash Detection & much more.",
+            manufacter: "Manufactured by iphone",
+            battery: "Battery Timing Approx 2.5 days",
+            price: "11 Lakh in PKR",
+        },
+    },
+
+    samsung: {
+        S202Simple: {
+            image: "images/samsung-s202-simple.jpeg",
+            name: "S202 Simple",
+            about: "The Galaxy S22 is better at taking photos in the dark. The Galaxy S22 and S22 Plus have both gotten a noticeable camera upgrade.",
+            manufacter: "Manufactured by samsung",
+            battery: "Battery Timing Approx 2 days",
+            price: "10,200 in PKR",
+        },
+        S202Ultra: {
+            image: "images/samsung-s202-ultra.jpeg",
+            name: "S2 02 Ultra",
+            about: "The Galaxy S2 02 is better at taking photos in the dark. The Galaxy S22 and S22 Plus have both gotten camera.",
+            manufacter: "Manufactured by samsung",
+            battery: "Battery Timing Approx 3 days",
+            price: "89,000 in PKR"
+        },
+    },
+
+    sony: {
+        "Xperia 5V": {
+            image: "images/sony-experia-5v-compact.jpg",
+            name: "Xperia 5V",
+            about: "The new Sony Xperia 5 V retains all the best features of previous Xperia phones, like a 4K high refresh rate display",
+            manufacter: "Manufactured by sony",
+            battery: "Battery Timing Approx 3 days",
+            price: "98,000 in PKR"
+        },
+
+        Xperia10IV: {
+            image: "images/sony-experia-10-IV.jpeg",
+            name: "Xperia 10 IV",
+            about: "The Xperia 10 IV also boasts a triple camera setup. This includes a 12-megapixel main camera, 8MP ultrawide sensor.",
+            manufacter: "Manufactured by samsung",
+            battery: "Battery Timing Approx 1.5 days",
+            price: "109,000 in PKR"
+        },
+    },
+
+    LG: {
+        LGUltraTab: {
+            image: "images/lg-ulta-tab.jpeg",
+            name: "LG Ultra Tab",
+            about: "LG Ultra Tab Android tablet. Announced Aug 2022. Features 10.35″ display,Snapdragon 680 4G chipset.",
+            manufacter: "Manufactured by LG",
+            battery: "Battery Timing Approx 1.5 days",
+            price: "91,000 in PKR"
+        },
+
+        LGW41Pro: {
+            image: "images/lg-w41-pro.jpeg",
+            name: "LG W41 Pro",
+            about: "LG W41 Pro is officially announced on February 22, 2021. The smartphone comes in a dimension of 9.3 mm.",
+            manufacter: "Manufactured by LG",
+            battery: "Battery Timing Approx 2.5 days",
+            price: "111,999 in PKR"
+        },
+    },
+
+    Vivo: {
+        VivoX80: {
+            image: "images/vivo-x80.jpg",
+            name: "Vivo X80",
+            about: "There is a 50 MP main camera, with OIS and laser autofocus, a 12 MP 2X telephoto, and a 12 MP ultrawide.",
+            manufacter: "Manufactured by Vivo",
+            battery: "Battery Timing Approx 3 days",
+            price: "99,999 in PKR"
+        },
+
+        VivoY27: {
+            image:  "images/vivo-y27.jpeg",
+            name: "Vivo Y27",
+            about: "Y27 is equipped with a vivo standard charger (power adapter 11V/4A), and supports up to 44W FlashCharge.",
+            manufacter: "Manufactured by Vivo",
+            battery: "Battery Timing Approx 3 days",
+            price: "89,999 in PKR"
+        },
+    },
+
+    Xiaomi: {
+        xiaomi10Pro: {
+            image:  "images/xiaomi-10-pro.webp",
+            name: "Xiaomi 10 Pro",
+            about: "Xiaomi 12 Pro Android smartphone. Announced Dec 2021. Features 6.73″ display, Snapdragon 8 Gen 1 chipset, 4600 mAh battery.",
+            manufacter: "Manufactured by Xiaomi",
+            battery: "Battery Timing Approx 2 days",
+            price: "69,999 in PKR"
+        },
+
+        Xiaomi13T: {
+            image: "images/xiaomi-13t.webp",
+            name: "Xiaomi 10 Pro",
+            about: "Xiaomi 13T Pro Android smartphone. Announced Dec 2021. Features 6.73″ display, Snapdragon 8 Gen 1 chipset, 4600 mAh battery.",
+            manufacter: "Manufactured by Xiaomi",
+            battery: "Battery Timing Approx 2 days",
+            price: "75,999 in PKR",
+           
+        },
+    },
+
+    Google: {
+        GooglePixel7a: {
+            image: "images/google-pixel-7a.jpeg",
+            name: "Google Pixel 7a",
+            about: "The Pixel 7A has two rear cameras housed in a distinctive camera bar that spans then entire width of the phone",
+            manufacter: "Manufactured by Google",
+            battery: "Battery Timing Approx 2.5 days",
+            price: "206,50 in PKR",
+           
+        },
+
+        GooglePixel7Pro: {
+            image: "images/Google-Pixel-7-Pro.jpg" ,
+            name: "Google Pixel 7 Pro",
+            about: "the Pixel 7 Pro seems to have it all: a big, 6.7-inch display with a 120Hz refresh rate, a large 5,000mAh battery.",
+            manufacter: "Manufactured by Google",
+            battery: "Battery Timing Approx 3 days",
+            price: "106,500 in PKR",
+        },
+    },
+
+    Nokia: {
+        NokiaX20: {
+            image: "images/nokia-x20.jpeg",
+            name: "Nokia X20",
+            about: "Nokia X20 is built to last, and because you'll have it for a while, we knew it had to be good & 6700 mAh battery.",
+            manufacter: "Manufactured by Nokia",
+            battery: "Battery Timing Approx 4 days",
+            price: "105,500 in PKR",
+        },
+
+        NokiaX10: {
+            image: "images/nokia-x10.jpeg",
+            name: "Nokia X10",
+            about: "Nokia X10 is built to last, and because you'll have it for a while, we knew it had to be good & 6700 mAh battery & as much more features you want like your favorite movies or music.",
+            manufacter: "Manufactured by Nokia",
+            battery: "Battery Timing Approx 3 days",
+            price: "100,500 in PKR",
+        },
+    },
+
+    Realme: {
+        Realme9ProPlus: {
+            image: "images/realme-p-pr0-plus.jpeg",
+            name: "Realme 9 Pro Plus",
+            about: "The realme 9 Pro+ lets you have two 5G SIM cards running simultaneously. 5G connectivity also means you can download your favorite movies or music at lightning-fast speeds",
+            manufacter: "Manufactured by Realme",
+            battery: "Battery Timing Approx 2 days",
+            price: "90,500 in PKR",
+        },
+
+        Realme9i: {
+            image: "images/rralme-9i.jpeg",
+            name: "Realme 9i",
+            about: "The realme 9i lets you have two 4G SIM cards running simultaneously. 5G connectivity also means you can download your favorite movies or music at lightning-fast speeds & much more.",
+            manufacter: "Manufactured by Realme",
+            battery: "Battery Timing Approx 3 days",
+            price: "92,500 in PKR",
+        },
+    },
+
+    Infinix: {
+        InfinixZero30: {
+            image: "images/infinix-zero-30.jpg" ,
+            name: "Infinix Zero 30",
+            about: "Infinix Zero 30 Android smartphone. Announced Sep 2023. Features 6.78″ display,Dimensity 8020 chipset, 5000 mAh battery, 256 GB storage, 12 GB RAM & much more you want.",
+            manufacter: "Manufactured by Infinix",
+            battery: "Battery Timing Approx 3.5 days",
+            price: "99,500 in PKR",
+        },
+
+        InfinixGT10Pro: {
+            image:"images/infinix-GT-10-pro.jpeg" ,
+            name: "Infinix GT 10 Pro",
+            about: "Infinix GT 10 Pro Android smartphone. Announced Sep 2023. Features 6.78″ display,Dimensity 8020 chipset, 5000 mAh battery, 256 GB storage, 12 GB RAM & much more you want.",
+            manufacter: "Manufactured by Infinix",
+            battery: "Battery Timing Approx 4 days",
+            price: "110,500 in PKR",
+        },
+    },
+};
+
+var company = document.getElementById("company");
+var brand = document.getElementById("brand");
+var allMobiles = document.getElementById("allMobile");
+
+company.innerHTML = `<option>search company</option>`
+brand.innerHTML = `<option>search model</option>`
+
+for (var key in mobileCompanies) {
+   
+    company.innerHTML += `
+  <option value="${key}">${key.toUpperCase()}</option>`;
+  for (var key1 in mobileCompanies[key]) {
+            allMobiles.innerHTML += `
+            <div class="col mb-3">
+            <div class="card">
+                <img src=${mobileCompanies[key][key1].image} class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${mobileCompanies[key][key1].name}</h5>
+                    <p class="card-text">${mobileCompanies[key][key1].about}</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${mobileCompanies[key][key1].manufacter}</li>
+                    <li class="list-group-item">${mobileCompanies[key][key1].battery}</li>
+                    <li class="list-group-item">${mobileCompanies[key][key1].price}</li>
+                </ul>
+            </div>
+
+        </div>
+            `
+
+        }
+    }
+
+
+
+function dailyUpdate() {
+
+    brand.innerHTML = "";
+    brand.innerHTML = `<option>search model</option>`
+
+    for (var key in mobileCompanies[company.value]) {
+        console.log(mobileCompanies)
+        brand.innerHTML += `
+        <option value="${key}">${key.toUpperCase()}</option>`
+    }
+};
+
+function filterMobile(){
+    
+    var mobileData = mobileCompanies[company.value][brand.value]
+
+    console.log("mobileData",mobileData)
+    allMobiles.innerHTML = `
+    <div class="my-div col-12 mb-3">
+    <div class="card">
+        <img style = "height: 600px; width:100%" src="${mobileData.image}" class="card-img-top img-fluid" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${mobileData.name}</h5>
+            <p class="card-text">${mobileData.about}</p>
+        </div>
+         <ul class="list-group list-group-flush">
+             <li class="list-group-item">${mobileData.manufacter}</li>
+             <li class="list-group-item">${mobileData.battery}</li>
+             <li class="list-group-item">${mobileData.price}</li>
+         </ul>
+    </div>
+
+</div>
+    `
+}
+
+
